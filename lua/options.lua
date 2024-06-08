b4b97b09443b4597c1832f14d8d9caae8c2f72c0
misononoa <example@example.com>
2024-06-08 10:35:50 +0900

    タブ幅とインデント幅の設定を修正

--- lua/options.lua
+++ lua/options.lua
@@ -1,17 +1,18 @@
---
--- 一般設定
---
 local api = vim.api
 local set_opt = api.nvim_set_option
 local set_win = api.nvim_win_set_option
+
+--
+-- 一般設定
+--
 set_opt("termguicolors", true)
 set_opt("scrolloff", 4)
 set_opt("inccommand", "split")
 set_opt("showtabline", 2)
+set_opt("tabstop", 3)
+set_opt("shiftwidth", 3)
 
 set_win(0, "number", true)
 set_win(0, "relativenumber", true)
 set_win(0, "wrap", false)
-set_win(0, "tabstop", 3)
-set_win(0, "shiftwidth", 3)
 set_win(0, "cursorline", true)
