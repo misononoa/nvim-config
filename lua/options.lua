57b77cf3a3afbc588067af1834550935b23c8799
misononoa <misononoa@example.com>
2024-06-15 00:38:05 +0900

    OSを判定する関数を用意

--- lua/options.lua
+++ lua/options.lua
@@ -1,4 +1,14 @@
 local set_opt = vim.api.nvim_set_option_value
+local is_os = function(os_name)
+	local has = vim.fn.has
+	if os_name == "win" then
+		return has("win32") == 1 or has("win64") == 1
+	elseif os_name == "linux" then
+		return has("linux") == 1
+	else
+		return false
+	end
+end
 
 --
 -- general
@@ -16,11 +26,14 @@ set_opt("wrap", false, {})
 set_opt("cursorline", true, {})
 
 -- 言語を英語に
-if vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1 then
+if is_os("win") then
 	vim.cmd [[language en_US]]
-elseif vim.fn.has("linux") == 1 then
+elseif is_os("linux") == 1 then
 	vim.cmd [[language C.utf8]]
 end
+
+-- ホームディレクトリに、移動しちゃいますよ～～～～
+vim.cmd [[cd ~]]
 
 --
 -- keymap
@@ -30,8 +43,12 @@ local opts = { noremap = true, silent = true }
 -- タブ操作
 map("n", "<C-Left>", "<cmd>tabprevious<cr>", opts)
 map("n", "<C-Right>", "<cmd>tabnext<cr>", opts)
-map("n", "<C-t><C-t>", "<cmd>tab term pwsh.exe<cr>", opts)
 map("n", "<C-t>", "<cmd>tabnew<cr>", opts)
+if is_os("win") then
+	map("n", "<C-t><C-t>", "<cmd>tab term pwsh.exe<cr>", opts)
+elseif is_os("linux") then
+	map("n", "<C-t><C-t>", "<cmd>tab term<cr>", opts)
+end
 -- ターミナルを抜ける操作
 map("t", "<Esc><Esc>", "<C-\\><C-n>", opts)
 
